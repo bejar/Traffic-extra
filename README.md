f3b0522ac638d58335778fe00e659a23eaf9a6b1
javier.bejar <bejar@cs.upc.edu>
2017-01-16 12:06:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Traffic extra
+=============
+
+Data retrieval and web monitoring of the training
---
+++ Util/__init__.py
@@ -0,0 +1,20 @@
+"""
+.. module:: __init__.py
+
+__init__.py
+*************
+
+:Description: __init__.py
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 16/01/2017 11:29 
+
+"""
+
+__author__ = 'bejar'
